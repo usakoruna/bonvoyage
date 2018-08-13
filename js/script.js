01d10523fc4f940572de0fc24945c914a0d73509
EC2 Default User <ec2-user@ip-172-31-15-53.us-east-2.compute.internal>
2018-08-13 06:06:15 +0000

    first commit

---
+++ js/script.js
@@ -0,0 +1,34 @@
+$( document ).ready(function() {
+  $( '#full' ).sliderPro({
+    width: '100%',//横幅
+   
+    buttons: false,//ナビゲーションボタン
+    slideDistance:0,//スライド同士の距離
+    aspectRatio: 1.5,//スライドのアスペクト比
+    forceSize: 'fullWindow',//ウィンドウ内全画面表示
+    breakpoints: {
+      480: {//表示方法を変えるサイズ
+         width: '100%',//横幅
+         aspectRatio: -1,//スライドのアスペクト比
+    　　forceSize: 'none',//ウィンドウ内全画面表示
+      }
+    } 
+    
+  });
+});
+
+
+ $(function(){
+    $(document).ready(function(){
+
+      $(".sp_navi_btn").click(function () {
+        $(".sp_navi_li").slideToggle();
+      });
+
+    });
+    $(".close").click(function(){
+        $(".sp_navi_li").slideToggle();
+    });
+  });
+  
+ new WOW().init();
