3a0bf83d5dc721eaff6b853bfa91a2201321d1f1
EC2 Default User <ec2-user@ip-172-31-15-53.us-east-2.compute.internal>
2018-08-14 06:22:14 +0000

    second commit

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ $( document ).ready(function() {
     slideDistance:0,//スライド同士の距離
     aspectRatio: 1.5,//スライドのアスペクト比
     forceSize: 'fullWindow',//ウィンドウ内全画面表示
+    fade: true,　// フェードインアウト
     breakpoints: {
       480: {//表示方法を変えるサイズ
          width: '100%',//横幅
